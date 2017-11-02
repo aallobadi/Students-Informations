99026dacd47453576baacc3b4c6a5c3e00ab4f7a
Amjad Allobadi <aallobadi@lcmail.lcsc.edu>
2017-11-02 16:31:30 -0700

    Adding main

---
+++ main.py
@@ -0,0 +1,121 @@
+# Lab 9
+def quickSort(alist):
+   quickSortHelper(alist,0,len(alist)-1)
+
+def quickSortHelper(alist,first,last):
+   if first<last:
+
+       splitpoint = partition(alist,first,last)
+
+       quickSortHelper(alist,first,splitpoint-1)
+       quickSortHelper(alist,splitpoint+1,last)
+
+
+def partition(alist,first,last):
+   pivotvalue = alist[first]
+
+   leftmark = first+1
+   rightmark = last
+
+   done = False
+   while not done:
+
+       while ((leftmark <= rightmark) and (alist[leftmark].get_id() <= pivotvalue.get_id())):
+           leftmark = leftmark + 1
+
+       while ((alist[rightmark].get_id() >= pivotvalue.get_id()) and (rightmark >= leftmark)):
+           rightmark = rightmark -1
+
+       if rightmark < leftmark:
+           done = True
+       else:
+           temp = alist[leftmark]
+           alist[leftmark] = alist[rightmark]
+           alist[rightmark] = temp
+
+   temp = alist[first]
+   alist[first] = alist[rightmark]
+   alist[rightmark] = temp
+
+
+   return rightmark
+
+def binarySearch(aList, sid):
+    start=0
+    end= len(aList)-1
+    done=False
+    count=0
+    while not done and start<=end:
+        mid=(start+end)//2
+        count+=1
+        if (sid > aList[mid].get_id()):
+            start=mid+1
+        elif (sid < aList[mid].get_id()):
+            end=mid-1
+        else:
+            done=True
+    if done==True:
+        return aList[mid].__str__()
+    else:
+        return "Not found."
+
+
+class Student(object):
+    def __init__(self, sid, gpa, age, credit):
+        self.sid = sid
+        self.gpa = gpa
+        self.age = age
+        self.credit = credit
+
+    def get_id(self):
+        return self.sid
+
+    def get_gpa(self):
+        return self.gpa
+
+    def get_age(self):
+        return self.age
+
+    def get_credit(self):
+        return self.credit
+    def __str__(self):
+       info = ("Student ID: %s\tCumulative GPA: %s\tAge: %s\tTotal Course Credits: %s" % (self.sid, self.gpa, self.age, self.credit))
+       return info
+
+    
+
+def main():
+    fp = open("LCSC-Student_Records.dat")
+    aList = []
+
+    while True:
+        line = fp.readline()
+
+        if line == "":
+            break
+
+        line = line.strip()
+        word = line.split()
+
+        student = Student(word[0],word[1],word[2],word[3])
+
+        aList.append(student)
+    #print(aList[1])
+    quickSort(aList)
+    #print(aList[1])
+
+    print(binarySearch(aList,"74261880"))
+    print(binarySearch(aList,"47355068"))
+    print(binarySearch(aList,"57518851"))
+    print(binarySearch(aList,"85055902"))
+    print(binarySearch(aList,"67183075"))
+
+    
+
+main()
+
+        
+
+    
+
+        
